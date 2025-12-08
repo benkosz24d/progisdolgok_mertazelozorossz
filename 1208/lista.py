a0be8bc6381afb8960cbf2f2c5fad4682895e6d9
benkosz24d <benkosz.24d@acsjszki.hu>
2025-12-08 08:46:46 +0100

    lista.py

---
+++ 1208/lista.py
@@ -0,0 +1,19 @@
+Lszamok=[1,2,3,4,5]
+szam=8
+
+"""
+print(Lszamok[0])
+print(Lszamok[1])
+print(len(Lszamok))
+
+#Utolsó elem kiíratása
+print(Lszamok[len(Lszamok)-1])
+
+last_index=len(Lszamok)-1
+print(Lszamok[last_index])
+
+print(Lszamok[-1])"""
+
+#Elemek kiíratása ciklussal
+for elem in Lszamok:
+    print(elem)
